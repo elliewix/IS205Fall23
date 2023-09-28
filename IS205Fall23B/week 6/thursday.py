51f8c1762d4806a03103f91da877b52206c7b8f2
Elizabeth Wickes <elliewix@gmail.com>
2023-09-28 11:43:02 -0500

    section b

---
+++ IS205Fall23B/week 6/thursday.py
@@ -0,0 +1,43 @@
+import string
+print(string.punctuation)
+words = "here's some __stuff__ *thing* )()"
+
+for punc in string.punctuation:
+    print(punc)
+    words = words.replace(punc, "")
+# words = words.replace(")", "")
+# words = words.replace("(", "")
+
+print(words)
+
+# the in keyword
+
+# alone, used as a boolean operator
+
+# when used with strings....
+# looks for substrings within
+print("hell" in "hello")
+
+# when used with a list on the right....
+
+phrase = "hello there      humans"
+words = phrase.split()
+print(words)
+print("hell" in words)
+
+
+#####
+
+def clean_punc(source_string):
+    # do stuff.....
+    
+    # presumably you've changed source_string....
+    return source_string
+
+t1 = "***START OF THE PROJECT GUTENBERG EBOOK THREE YEARS IN EUROPE***"
+t2 = "E-text prepared by Suzanne Shell, Michael Punch, and the Project Gutenberg"
+t3 = "MEMOIR OF WILLIAM WELLS BROWN,                           _Page_ ix-xxix"
+
+print(clean_punc(t1))
+print(clean_punc(t2))
+print(clean_punc(t3))
