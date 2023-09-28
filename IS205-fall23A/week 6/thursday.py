c8b4a5b229e073ed60d327031e89808fd72bbdf8
Elizabeth Wickes <elliewix@gmail.com>
2023-09-28 10:19:33 -0500

    thurs

---
+++ IS205-fall23A/week 6/thursday.py
@@ -0,0 +1,52 @@
+import string
+
+print(string.punctuation)
+
+word = "___hello. (There don't"
+for punc in string.punctuation:
+    word = word.replace(punc, "")
+
+print(word)
+
+phrase = "hello here     is some text"
+
+words = phrase.split()
+print(words)
+
+
+
+
+
+### hw2 starting point...
+
+def clean_punc(source_string):
+    # do stuff to it
+    new_string = "I'll fix this: " + source_string  #clearly this needs to change
+    return new_string
+
+t1 = "***START OF THE PROJECT GUTENBERG EBOOK THREE YEARS IN EUROPE***"
+t2 = "E-text prepared by Suzanne Shell, Michael Punch, and the Project Gutenberg"
+t3 = "MEMOIR OF WILLIAM WELLS BROWN,                           _Page_ ix-xxix"
+
+print(clean_punc(t1))
+print(clean_punc(t2))
+print(clean_punc(t3))
+
+
+
+### in keyword
+
+phrase = "hello here     is some text"
+
+words = phrase.split()
+print(words)
+
+# in with a string, it'll look for substring
+
+print("hell" in "hello")
+
+# in with a list looks for EXACT match in the members
+phrase = "hello here     is some text"
+
+words = phrase.split()
+print("hell" in words)
